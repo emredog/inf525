a05a42429dfc686e639b391860147b2a166a9240
emredog <emredog@gmail.com>
2014-03-04 11:34:33 +0200

    Compare manual gray transformation vs. Mat::convertTo() method

--- OpenCV-ContrastBrightnessExample/main.cpp
+++ OpenCV-ContrastBrightnessExample/main.cpp
@@ -9,18 +9,46 @@ int main(/*int argc, char *argv[]*/)
     double alpha = 1.0;
     double beta = 100;
 
+    //read image from disk
     Mat origImg = imread("/home/emredog/Pictures/Lenna.png");
-    Mat modifiedImg, convertedImg;
 
-    namedWindow("org", 1);
-    namedWindow("new", 1);
-    namedWindow("converted", 1);
+    //create a zero-matrix with original image size & type
+    Mat modifiedImg = Mat::zeros( origImg.size(), origImg.type() );
 
-    modifiedImg = origImg*alpha + beta;
+    // Do the operation new_image(i,j) = alpha*image(i,j) + beta
+    for( int y = 0; y < origImg.rows; y++ ) //traverse rows
+    {
+        for( int x = 0; x < origImg.cols; x++ ) //traverse columns
+        {
+            for( int c = 0; c < 3; c++ ) //traverse RGB channels
+            {
+                modifiedImg.at<Vec3b>(y,x)[c] = saturate_cast<uchar>( alpha*( origImg.at<Vec3b>(y,x)[c] ) + beta );
+            }
+        }
+    }
+
+    //create an empty matrix
+    Mat convertedImg;
+    //easy way to do the same operation
     origImg.convertTo(convertedImg, -1, alpha, beta);
 
+    //create three windows to show images
+    namedWindow("org", 1);
+    namedWindow("converted", 1);
+    namedWindow("modified", 1);
+
+    //verify output images are equal
+    Mat resultMask = (convertedImg == modifiedImg);
+    for (int y=0; y<resultMask.rows; y++)
+        for (int x=0; x<resultMask.cols; x++)
+            if (resultMask.at<uchar>(y, x) != 255)
+            {
+                std::cout << "IMAGES NOT EQUAL" << std::endl;
+            }
+
+    //show images
     imshow("org", origImg);
-    imshow("new", modifiedImg);
+    imshow("modified", modifiedImg);
     imshow("converted", convertedImg);
     cv::waitKey();
     return 0;
